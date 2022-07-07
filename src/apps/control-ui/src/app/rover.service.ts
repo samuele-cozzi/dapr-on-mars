24295a18b1f6ebff1939d11a7f1a2efb0b856f17
samuele-cozzi <samuele.cozzi@gmail.com>
2022-07-07 22:45:52 +0200

    add darp client

--- src/apps/control-ui/src/app/rover.service.ts
+++ src/apps/control-ui/src/app/rover.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { environment } from '../environments/environment';
-//import { DaprClient, HttpMethod } from "dapr-client"; 
+import { DaprClient, HttpMethod } from "dapr-client"; 
 
 import { RoverPosition } from './rover';
 
---
+++ src/apps/control-ui/src/index.d.ts
@@ -0,0 +1,5 @@
+//declare module 'node-fetch';
+//declare module 'google-protobuf/google/protobuf/any_pb';
+
+//declare module 'google-protobuf';
+//declare module '*';
